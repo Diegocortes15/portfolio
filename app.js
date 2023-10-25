46e7101f972cdc04359909fe0a38da86021ff1cd
Diego Cortes <cortesroadiegoalejandro@gmail.com>
2023-10-24 21:30:31 -0500

    main: Update project

--- app.js
+++ app.js
@@ -1,4 +1,60 @@
-"use-strict";
+"use strict";
+
+const words = document.querySelectorAll(".rol-word");
+const wordArray = [];
+let currentWord = 0;
+
+words[currentWord].style.opacity = 1;
+for (let i = 0; i < words.length; i++) {
+  splitLetters(words[i]);
+}
+
+function changeWord() {
+  const cw = wordArray[currentWord];
+  const nw =
+    currentWord == words.length - 1 ? wordArray[0] : wordArray[currentWord + 1];
+  for (let i = 0; i < cw.length; i++) {
+    animateLetterOut(cw, i);
+  }
+
+  for (let i = 0; i < nw.length; i++) {
+    nw[i].className = "letter behind";
+    nw[0].parentElement.style.opacity = 1;
+    animateLetterIn(nw, i);
+  }
+
+  currentWord = currentWord == wordArray.length - 1 ? 0 : currentWord + 1;
+}
+
+function animateLetterOut(cw, i) {
+  setTimeout(function () {
+    cw[i].className = "letter out";
+  }, i * 80);
+}
+
+function animateLetterIn(nw, i) {
+  setTimeout(function () {
+    nw[i].className = "letter in";
+  }, 340 + i * 80);
+}
+
+function splitLetters(word) {
+  const content = word.innerHTML;
+  word.innerHTML = "";
+  const letters = [];
+  for (let i = 0; i < content.length; i++) {
+    const letter = document.createElement("span");
+    letter.className = "letter";
+    letter.innerHTML = content.charAt(i) === " " ? "&nbsp;" : content.charAt(i);
+    word.appendChild(letter);
+    letters.push(letter);
+  }
+
+  wordArray.push(letters);
+}
+
+changeWord();
+setInterval(changeWord, 6000);
 
 const projects = document.querySelectorAll(".project__box");
 
@@ -8,14 +64,12 @@ projects.forEach((project) => {
   const aspectRatio = width / height;
   let animationFrameId = null;
 
-  project.addEventListener("mousemove", (evt) => {
+  project.addEventListener("mousemove", ({ layerX, layerY }) => {
     if (animationFrameId) {
       cancelAnimationFrame(animationFrameId);
     }
 
     animationFrameId = requestAnimationFrame(() => {
-      const { layerX, layerY } = evt;
-
       let yRotation = ((layerX - width / 2) / width) * 25;
       let xRotation = ((layerY - height / 2) / height) * 25;
 
@@ -25,7 +79,7 @@ projects.forEach((project) => {
       }
 
       const transformString = `
-        perspective(50rem)
+        perspective(70rem)
         scale(1.1)
         rotateX(${xRotation}deg)
         rotateY(${yRotation}deg)`;
@@ -39,10 +93,21 @@ projects.forEach((project) => {
       cancelAnimationFrame(animationFrameId);
     }
 
-    project.style.transform = `
-      perspective(50rem)
-      scale(1)
-      rotateX(0)
-      rotateY(0)`;
+    project.style.transform = "";
   });
 });
+
+const projectObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("project-show");
+      setTimeout(() => {
+        entry.target.classList.remove("project-hidden");
+      }, 500);
+    } else {
+      entry.target.classList.add("project-hidden");
+    }
+  });
+});
+const projectsHidden = document.querySelectorAll(".project-hidden");
+projectsHidden.forEach((project) => projectObserver.observe(project));

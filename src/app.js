340aa7a8585a437296429a9490d6fb294720c646
Diego Cortes <cortesroadiegoalejandro@gmail.com>
2023-10-30 03:16:37 -0500

    main: Delete setTimeout in preloader script in app.js

--- src/app.js
+++ src/app.js
@@ -47,70 +47,13 @@ document.addEventListener("mouseout", (e) => {
 
 /** Preloader */
 
-const preloader = document.querySelector("#preloader");
-window.addEventListener("load", function () {
-  setTimeout(() => {
+window.addEventListener("load", () => {
+  // "load" event has occurred
+  const preloader = document.querySelector("#preloader");
+  if (preloader) {
     preloader.style.display = "none";
-  }, 1000);
+  }
 });
-
-/** Words */
-
-/*const words = document.querySelectorAll(".rol-word");
-const wordArray = [];
-let currentWord = 0;
-
-words[currentWord].style.opacity = 1;
-for (let i = 0; i < words.length; i++) {
-  splitLetters(words[i]);
-}
-
-function changeWord() {
-  const cw = wordArray[currentWord];
-  const nw =
-    currentWord == words.length - 1 ? wordArray[0] : wordArray[currentWord + 1];
-  for (let i = 0; i < cw.length; i++) {
-    animateLetterOut(cw, i);
-  }
-
-  for (let i = 0; i < nw.length; i++) {
-    nw[i].className = "letter behind";
-    nw[0].parentElement.style.opacity = 1;
-    animateLetterIn(nw, i);
-  }
-
-  currentWord = currentWord == wordArray.length - 1 ? 0 : currentWord + 1;
-}
-
-function animateLetterOut(cw, i) {
-  setTimeout(function () {
-    cw[i].className = "letter out";
-  }, i * 80);
-}
-
-function animateLetterIn(nw, i) {
-  setTimeout(function () {
-    nw[i].className = "letter in";
-  }, 340 + i * 80);
-}
-
-function splitLetters(word) {
-  const content = word.innerHTML;
-  word.innerHTML = "";
-  const letters = [];
-  for (let i = 0; i < content.length; i++) {
-    const letter = document.createElement("span");
-    letter.className = "letter";
-    letter.innerHTML = content.charAt(i) === " " ? "&nbsp;" : content.charAt(i);
-    word.appendChild(letter);
-    letters.push(letter);
-  }
-
-  wordArray.push(letters);
-}
-
-changeWord();
-setInterval(changeWord, 6000);*/
 
 /** Projects Reveal */
 
